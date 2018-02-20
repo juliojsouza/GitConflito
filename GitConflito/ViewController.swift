4ea6db8977a1663ad5fffb690bbbedb8f30cc417
Júlio Souza <juliojsouza2@globo.com>
2018-02-19 21:44:17 -0300

    marge das alterações

--- GitConflito/ViewController.swift
+++ GitConflito/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Adoramos café, ou não
         // Teste Conflito
         self.mostrarOla()
     }
