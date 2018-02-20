be8785e98e62d9beef422f2cb1cd8adeb7bd6a1a
Júlio Souza <juliojsouza@icloud.com>
2018-02-19 21:13:21 -0300

    Implementação da Função MostrarOla

--- GitConflito/ViewController.swift
+++ GitConflito/ViewController.swift
@@ -12,7 +12,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Teste Conflito
+        self.mostrarOla()
+    }
+    
+    func mostrarOla() {
+        print("Olá")
     }
 
     override func didReceiveMemoryWarning() {
